d42cb257956670debf8cbcb7c913cf7b98d09434
Sambhav1102 <140238161+Sambhav1102@users.noreply.github.com>
2023-08-17 23:49:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import { SearchPage } from './pages/SearchPage';
+import './styles.css'
+function App() {
+  return (
+    <SearchPage/>
+  );
+}
+
+export default App;
---
+++ src/components/Songs.jsx
@@ -0,0 +1,9 @@
+import { Song } from "./Song"
+
+export const Songs=({allsongs,fn})=>{
+    return(<>
+
+    {allsongs.map((currentSong,index)=><Song fn={fn}key={index} song={currentSong}/>)}
+    
+    </>)
+}
