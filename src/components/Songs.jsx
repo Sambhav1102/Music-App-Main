4a6d87d5f4c78a9b71a16b62a2c72f9a666bd523
Sambhav1102 <140238161+Sambhav1102@users.noreply.github.com>
2023-08-26 13:39:39 +0530

    Delete src directory

--- src/components/Songs.jsx
+++
@@ -1,9 +0,0 @@
-import { Song } from "./Song"
-
-export const Songs=({allsongs,fn})=>{
-    return(<>
-
-    {allsongs.map((currentSong,index)=><Song fn={fn}key={index} song={currentSong}/>)}
-    
-    </>)
-}
